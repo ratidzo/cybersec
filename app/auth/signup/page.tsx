21ba3ae5e8deee3817e989ff987d8f4e10f232b9
ratidzo <ratygore@gmail.com>
2024-01-16 00:13:26 +0200

    dev: setup form fields and styles

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 
 const SignupPage = () => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 place-items-center items-center gap-4">
             <div className="md:col-span-2 flex justify-center items-center">
             <p className="text-centr p-2">Already Signed up?</p>
             <Link href={"/auth/signin"}>Sign In</Link>
--- app/components/SignupForm.tsx
+++ app/components/SignupForm.tsx
@@ -1,8 +1,45 @@
+"use client"
+
+import { Checkbox, Input, Button } from "@nextui-org/react"
+import { UserIcon, EnvelopeIcon, PhoneIcon, KeyIcon, EyeIcon, EyeSlashIcon } from "@heroicons/react/20/solid"
+import { useState } from "react"
+import Link from "next/link"
 
 
 const SignupForm = () => {
+    const [isVisiblePass, setIsVisiblePass] = useState(false)
+    const toggleVisiblePass = () => setIsVisiblePass(!isVisiblePass)
     return (
-        <div>Signup Form</div>
+        <form className="grid grid-cols-2 place-self-stretch gap-3 p-2 shadow border rounded-md">
+            <Input label="First Name" startContent={<UserIcon className="w-4" /> }/>
+            <Input label="Last Name" startContent={<UserIcon className="w-4" /> }/>
+            <Input
+             className="col-span-2"
+             label="Email" startContent={<EnvelopeIcon className="w-4" /> }/>
+            <Input
+             className="col-span-2"
+             label="Phone" startContent={<PhoneIcon className="w-4" /> }/>
+             <Input
+             className="col-span-2"
+             type={isVisiblePass ? "text": "password"}
+             label="Password" 
+             startContent={<KeyIcon className="w-4" /> }
+             endContent={
+                isVisiblePass? <EyeSlashIcon className="w-4 cursor-pointer" onClick={toggleVisiblePass}/>:
+                <EyeIcon className="w-4 cursor-pointer" onClick={toggleVisiblePass} />
+             }
+             />
+             <Input
+             className="col-span-2"
+             type={isVisiblePass ? "text": "password"}
+             label="Confirm Password" 
+             startContent={<KeyIcon className="w-4" /> }             
+             />
+             <Checkbox className="col-span-2">I Accept The <Link href={"/terms"}>Terms</Link></Checkbox>
+             <div className="col-span-2 py-2 flex justify-center">
+                <Button className="w-48" color="primary" type="submit">Submit</Button>
+             </div>
+        </form>
     )
 }
 

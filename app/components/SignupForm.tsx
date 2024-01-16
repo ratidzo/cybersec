05f2ee7fa7b162c24a5098c7bb3a9df91dbf52a6
ratidzo <ratygore@gmail.com>
2024-01-16 08:39:10 +0200

    dev: setup form validation error messages

--- app/components/SignupForm.tsx
+++ app/components/SignupForm.tsx
@@ -7,6 +7,7 @@ import Link from "next/link"
 import { z } from "zod"
 import validator from "validator"
 import { Controller, SubmitHandler, useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
 
 // create our form schema
 const FormSchema = z.object({
@@ -45,13 +46,15 @@ const FormSchema = z.object({
 
 }).refine(data => data.password === data.confirmPassword, {
     message: "Passwords don't match.",
-    path: ["password", "confirmPassword"]
+    path: ["confirmPassword"]
 })
 
 type InputType = z.infer<typeof FormSchema>
 
 const SignupForm = () => {
-    const {register, handleSubmit, reset, control } = useForm<InputType>()
+    const {register, handleSubmit, reset, control, formState: {errors} } = useForm<InputType>({
+        resolver: zodResolver(FormSchema)
+    })
     const [isVisiblePass, setIsVisiblePass] = useState(false)
     const toggleVisiblePass = () => setIsVisiblePass(!isVisiblePass)
     const saveUser: SubmitHandler<InputType> = async (data) => {
@@ -60,28 +63,38 @@ const SignupForm = () => {
     return (
         <form onSubmit={handleSubmit(saveUser)} className="grid grid-cols-2 place-self-stretch gap-3 p-2 shadow border rounded-md">
             <Input 
+            errorMessage={errors.firstName?.message}
+            isInvalid={!!errors.firstName}
             {...register("firstName")} 
             label="First Name" 
             startContent={<UserIcon className="w-4" /> }
             />
             <Input 
+            errorMessage={errors.lastName?.message}
+            isInvalid={!!errors.lastName}
             {...register("lastName")} 
             label="Last Name" 
             startContent={<UserIcon className="w-4" /> }
             />
             <Input 
+             errorMessage={errors.email?.message}
+             isInvalid={!!errors.email}
              {...register("email")}
              className="col-span-2"
              label="Email" 
              startContent={<EnvelopeIcon className="w-4" /> }
              />
             <Input 
+             errorMessage={errors.phone?.message}
+             isInvalid={!!errors.phone}
              {...register("phone")}
              className="col-span-2"
              label="Phone" 
              startContent={<PhoneIcon className="w-4" /> }
              />
              <Input
+             errorMessage={errors.password?.message}
+             isInvalid={!!errors.password}
              {...register("password")}
              className="col-span-2"
              type={isVisiblePass ? "text": "password"}
@@ -93,6 +106,8 @@ const SignupForm = () => {
              }
              />
              <Input
+             errorMessage={errors.confirmPassword?.message}
+             isInvalid={!!errors.confirmPassword}
              {...register("confirmPassword")}
              className="col-span-2"
              type={isVisiblePass ? "text": "password"}
@@ -107,6 +122,7 @@ const SignupForm = () => {
                         onChange={field.onChange} onBlur={field.onBlur}
                         className="col-span-2">I Accept The <Link href={"/terms"}>Terms</Link></Checkbox>
                 )} />
+                {!!errors.accepted && <p className="text-red-500 text-sm">{errors.accepted.message}</p>}
              
              <div className="col-span-2 py-2 flex justify-center">
                 <Button className="w-48" color="primary" type="submit">Submit</Button>

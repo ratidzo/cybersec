f68d950c620a0b9e3ff4ef66baf06faf01496787
ratidzo <ratygore@gmail.com>
2024-01-16 09:39:59 +0200

    dev: store password as hash - with salt rounds

--- app/lib/actions/authActions.ts
+++ app/lib/actions/authActions.ts
@@ -2,9 +2,13 @@
 
 import { User } from "@prisma/client"
 import prisma from "../prisma"
+import * as bycrypt from "bcrypt"
 
 export async function registerUser(user: Omit< User, "id" | "emailVerified" | "image" >) {
     const result = await prisma.user.create({
-        data: user,
+        data: {
+            ...user,
+            password: await bycrypt.hash(user.password, 10),
+        },
     })
 }
